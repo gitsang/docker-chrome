c4774581446e34d041c3fc10730329897e32d0d4
sang <sang.chen@outlook.com>
2025-11-04 11:39:55 +0800

    Update build script to remove rolling image tagging and pushing

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 
 dockerfile=$(find . -maxdepth 1 -name "Dockerfile.*" -type f -printf "%f\n" | sort -r | fzf --height 40% --layout reverse)
 chrome_version=${dockerfile##*_}
-image_name="gitsang/chrome:${chrome_version}-$(date +%Y%m%d)"
-rolling_image_name="gitsang/chrome:${chrome_version}-$(date +%Y%m%d)"
+image_name="gitsang/chrome:${chrome_version}"
 
 docker build -f "${dockerfile}" -t "${image_name}" .
-docker tag "${image_name}" "${rolling_image_name}"
 docker push "${image_name}"
-docker push "${rolling_image_name}"
